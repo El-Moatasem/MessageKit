a3cd3ef7dbbe8d34eb20a7116ee9507982a91768
El-Moatasem <el.moatasem.mohamed@gmail.com>
2021-08-13 14:54:26 +0200

    [ADD] Adding Audio Seeking Bar

--- Sources/Views/Cells/AudioMessageCell.swift
+++ Sources/Views/Cells/AudioMessageCell.swift
@@ -166,3 +166,19 @@ open class AudioMessageCell: MessageContentCell {
         displayDelegate.configureAudioCell(self, message: message)
     }
 }
+
+
+extension AudioMessageCell {
+    func makeCircleWith(size: CGSize, backgroundColor: UIColor) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(backgroundColor.cgColor)
+        context?.setStrokeColor(UIColor.clear.cgColor)
+        let bounds = CGRect(origin: .zero, size: size)
+        context?.addEllipse(in: bounds)
+        context?.drawPath(using: .fill)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
